d1bfe7417b9a27978e47d90ca57d8125d1c3bbca
Emre Sabancı <ecsabanci@users.noreply.github.com>
2021-01-27 20:49:08 +0300

    add changes

--- script.js
+++ script.js
@@ -6,6 +6,22 @@ const submitBtn = document.querySelector("#submit-btn");
 
 submitBtn.addEventListener("submit", (e) => {
 
-    window.location.href = "page-2.html";
+    location.href = "./page-2.html";
         
 });
+
+
+const backBtn = document.querySelector("#back-btn");
+
+const confirmBtn = document.querySelector("#confirm-btn");
+
+backBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    window.location.href = "./index.html";
+});
+
+
+confirmBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    window.location.href = "./page-3.html";
+})
--- script2.js
+++ script2.js
@@ -1,6 +1,14 @@
 const backBtn = document.querySelector("#back-btn");
 
+const confirmBtn = document.querySelector("#confirm-btn");
+
 backBtn.addEventListener("click", (e) => {
     e.preventDefault();
-    window.location.href = "index.html";
+    window.location.href = "./index.html";
 });
+
+
+confirmBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    window.location.href = "./page-3.html";
+})
